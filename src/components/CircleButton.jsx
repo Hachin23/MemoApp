5ca9d21479953dc78236bf627d807e58848e9a11
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-07 22:13:25 +0900

    Extract components

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
+
+export default function CircleButton(props) {
+    //分割代入
+    const { children } = props;
+    return (
+        <View style={ styles.circleButton }>
+            <Text style={ styles.circleButtonLabel }>{ children }</Text>
+        </View>
+    );
+}
+
+CircleButton.popTypes = {
+    children: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+    circleButton: {
+        backgroundColor: '#467FD3',
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        /** shadowプロパティはIOSのみ対応 */
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 0.8 },
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        /** elevationプロパティはアンドロイドのみ対応 */
+        elevation: 8,
+      },
+      circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 40,
+      },
+});
