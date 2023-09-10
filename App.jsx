7ebbf5af91288abd02fa19f39b86d34ef7fff93b
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-10 22:45:12 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen';
-//import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 
 // Reactの開発ではタブ幅=2スペースが推奨されているためSettingを修正する。
 export default function App() {
   return (
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
