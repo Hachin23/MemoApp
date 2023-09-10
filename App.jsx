2859e777adfb2c3ebad2d1c6dcb73ce51a3c957e
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-10 12:08:10 +0900

    Install Expo/vector-icons

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+//import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
+
+// Reactの開発ではタブ幅=2スペースが推奨されているためSettingを修正する。
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoListScreen />
   );
 }
--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -5,22 +5,22 @@ import AppBar from '../components/AppBar';
 import CircleButton from "../components/CircleButton";
 
 export default function MemoDetailScreen() {
-    return (
-        <View style={ styles.container }>
-            <AppBar />
-            <View style={ styles.memoHeader }>
-                <Text style={ styles.memoTitle }>買い物リスト</Text>
-                <Text style={ styles.memoDate }>2020年12月24日 10:00</Text>
-            </View>
-            <ScrollView style={ styles.memoBody }>
-                <Text style={ styles.memoText }>
-                    買い物リスト
-                    書体やレイアウトなどを確認するために用います。
-                    本文用なので使い方を間違えると不自然にみえることもありますので要注意。
-                </Text>
-            </ScrollView>
-            <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+return (
+<View style={ styles.container }>
+        <AppBar />
+        <View style={ styles.memoHeader }>
+            <Text style={ styles.memoTitle }>買い物リスト</Text>
+            <Text style={ styles.memoDate }>2020年12月24日 10:00</Text>
         </View>
+        <ScrollView style={ styles.memoBody }>
+            <Text style={ styles.memoText }>
+                買い物リスト
+                書体やレイアウトなどを確認するために用います。
+                本文用なので使い方を間違えると不自然にみえることもありますので要注意。
+            </Text>
+        </ScrollView>
+        <CircleButton style={{ top: 160, bottom: 'auto'}} name='edit-2'></CircleButton>
+    </View>
     );
 };
 
