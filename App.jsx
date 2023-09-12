790691c6247aec7d88d172598ec1ade419a2b202
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-12 20:22:01 +0900

    Create LogInScreen

--- App.jsx
+++ App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+//import MemoEditScreen from './src/screens/MemoEditScreen';
 //import MemoCreateScreen from './src/screens/MemoCreateScreen';
-
+import LogInScreen from './src/screens/LogInScreen';
 
 // Reactの開発ではタブ幅=2スペースが推奨されているためSettingを修正する。
 export default function App() {
   return (
-    <MemoEditScreen />
+    <LogInScreen />
   );
 }
