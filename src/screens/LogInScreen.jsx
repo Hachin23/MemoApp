68910a38555b23b2e0af58951c7b58b2e584a861
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-17 11:38:22 +0900

    Auto login and logout button

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View, Text, TextInput, StyleSheet, TouchableOpacity, Alert,
 } from 'react-native';
@@ -10,6 +10,21 @@ export default function LogInScreen(props) {
   const { navigation } = props;
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      }
+    });
+    //unmountされる瞬間に監視状態がキャンセルされる
+    return unsubscribe;
+  }, []);
+  //監視対象の値を入れておけば監視してくれる。
+  //空([])にすると画面を開いたときに一度だけ動いてくれる。いれないとpropsが変更されるたびに実行される。
 
   function handlePress() {
     firebase.auth().signInWithEmailAndPassword(email, password)
