e8d81aae93257ce43d2268a7298e0a02bcaf4608
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-18 20:02:59 +0900

    Create Loading component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -5,11 +5,13 @@ import {
 import firebase from 'firebase';
 
 import Button from "../components/Button";
+import Loading from "../components/Loading";
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -18,6 +20,8 @@ export default function LogInScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     //unmountされる瞬間に監視状態がキャンセルされる
@@ -27,6 +31,7 @@ export default function LogInScreen(props) {
   //空([])にすると画面を開いたときに一度だけ動いてくれる。いれないとpropsが変更されるたびに実行される。
 
   function handlePress() {
+    setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredentiall) => {
         const { user } = userCredentiall;
@@ -39,11 +44,16 @@ export default function LogInScreen(props) {
       .catch((error) => {
         console.log(error.code, error.message);
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
+
   }
 
   return (
     <View style={ styles.container}>
+      <Loading isLoading={isLoading}/>
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
