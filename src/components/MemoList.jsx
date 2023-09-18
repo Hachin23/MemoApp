01ab687a7b3c398e478bcb2d73f3ac7cee8a5e1d
masahito <74802233+Hachin23@users.noreply.github.com>
2023-09-18 10:50:31 +0900

    Update memo data on MemoEditScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -5,6 +5,8 @@ import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
 import { shape, string, instanceOf, arrayOf } from 'prop-types';
 
+import { dateToString } from '../utils';
+
 //アイコンが塗りつぶされるので、Featherアイコンで進める
 //import Icon from "./Icon";
 
@@ -20,7 +22,7 @@ export default function MemoList(props) {
       >
         <View>
             <Text style={ styles.memoListItemTitle } numberOfLines={1}>{item.bodyText}</Text>
-            <Text style={ styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+            <Text style={ styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 
 export function dateToString(date) {
   if (!date) { return ''; }
-  return format(date, 'yyyy年m月d日 HH時mm分');
+  return format(date, 'yyyy年MM月dd日 HH時mm分');
 }
 
 //export default と export(defaultなし)の違い
